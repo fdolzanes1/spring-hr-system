8ee9713d1abbf15ba5d95cd23f844b6b24e7e750
FabianoDolzanes <fdolzanes1@gmail.com>
2023-09-05 12:51:47 -0400

    first commit

---
+++ hr-user-api/src/main/java/com/dolzanes/HrUserApiApplication.java
@@ -0,0 +1,13 @@
+package com.dolzanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrUserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrUserApiApplication.class, args);
+	}
+
+}
---
+++ hr-user-api/src/test/java/com/dolzanes/HrUserApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dolzanes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrUserApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
