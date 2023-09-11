63651a428c9e2178c04469741ec9376f5f601b1a
FabianoDolzanes <fdolzanes1@gmail.com>
2023-09-11 14:24:13 -0400

    feat: added eureka server

--- hr-user-api/src/main/java/com/dolzanes/HrUserApiApplication.java
+++ hr-user-api/src/main/java/com/dolzanes/HrUserApiApplication.java
@@ -6,11 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import com.dolzanes.domain.entity.User;
 import com.dolzanes.domain.repository.UserRepository;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HrUserApiApplication implements CommandLineRunner {
 	
 	@Autowired
